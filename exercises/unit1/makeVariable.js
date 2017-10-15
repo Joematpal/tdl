33ecd3717a77a40eb8857f011b5928902d68ad2d
Joseph Palacio <josephmpalacio@gmail.com>
2017-10-15 16:24:32 -0500

    Add second lesson

---
+++ exercises/unit1/makeVariable.js
@@ -0,0 +1,15 @@
+/* Declare six variables one for each primate data type, string, number, boolean, null, undefined, symbol.
+ * Hint: Be sure to use the 'var' keyword followed by a 'name' of the variable, a.k.a. the variable identifier.
+ * 1: Call each one of your variables var1, var2, var3, var4, var5, var6.
+ * 2: For each one of your variables assign a 'string' respresenting the name of its respective data type.
+ * 3: The first one will be provided for you.
+ */
+
+// Example:
+var var1 = 'symbol';
+
+// Do work below here:
+
+ module.exports = {
+  var1, var2, var3, var4, var5, var6
+ }
